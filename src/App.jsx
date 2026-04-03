fba9ebe6bb0c54c8e280246a0cb979615a7cff51
lukakhaja <lukakhaja@yahoo.com>
2026-04-03 21:24:46 +0400

    bitasmbl add: Persistance

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,10 @@ function App() {
         <div>
           <h1>
             Get started, Hello ooo, again, and again yeah, and fucking agaaain!
-            looool and, guess, again! its never ending, haha, lol, bruh, insta
-            password, dunno, blyaaat, brah, fuck. damnnn, thats a no no, haha
-            lol, what good is bread, money in ur palm dont make u real, haha,
-            bitch
+            vibes in this bitxh~!! looool and, guess, again! its never ending,
+            haha, lol, bruh, insta password, dunno, blyaaat, brah, fuck. damnnn,
+            thats a no no, haha lol, what good is bread, money in ur palm dont
+            make u real, haha, bitch
           </h1>
           <p>
             Edit <code>src/App.jsx</code> and saveasdasd to test, bleeead, baby
